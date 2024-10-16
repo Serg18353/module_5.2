7d7e30b892efc1c93a98d531539d9a0828ce663a
Serg18353 <mikholenok@yandex.ru>
2024-10-16 16:23:20 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -6,11 +6,11 @@ class House:
         return self.numbers_of_floor
 
     def __str__(self):
-        return self.name
+        return f'Название: {self.name}, количество этажей: {self.numbers_of_floor}'
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print(h1)
-print(h1)
+print(h2)
 print(len(h1))
 print(len(h2))
